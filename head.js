da42d467fe7b6ba2ad53934cb39d556907907b62
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-20 14:25:24 -0700

    Separate testing within head.js to headTest.js

--- head.js
+++ head.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🎉 Assertion Passed: ${actual} === ${expected} 🎉`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected} ❌`);
-  }
-};
-
 const head = (anArray) => {
   if (anArray.length === 0) {
     return undefined;
@@ -14,8 +6,4 @@ const head = (anArray) => {
   }
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-assertEqual(head([]), undefined);
-assertEqual(head(['Solo']), 'Solo');
-assertEqual(head([5]), (5 + 1));                              // should fail
+module.exports = head;
