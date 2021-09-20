330ebe63f5756fb65844b06f5c7c0e5068440ebd
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-20 15:28:30 -0700

    Incorporate mocha and chai in tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,20 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-let testArray1 = [1, 2, 3, 4, 5];
-let testResult1 = tail(testArray1);
+describe('#tail', () => {
+  it('returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
 
-assertEqual(testResult1.length, 4);
-assertEqual(testResult1[0], 2);
-assertEqual(testResult1[1], 3);
-assertEqual(testResult1[2], 4);
-assertEqual(testResult1[3], 5);
+  it('returns [2] for [1, 2]', () => {
+    assert.deepEqual(tail([1, 2]), [2]);
+  });
+
+  it('returns [] for [\'hello\']', () => {
+    assert.deepEqual(tail(['hello']), []);
+  });
+
+  it('returns [] for []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
