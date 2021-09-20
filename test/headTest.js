e066f402906916ea52f3a6f4396e67c0dfe9ceff
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-20 15:14:53 -0700

    Incorporate mocha and chai in headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,13 @@
-const head = require('../head');
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
+const head = require('../head');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-assertEqual(head([]), undefined);
-assertEqual(head(['Solo']), 'Solo');
-assertEqual(head([5]), (5 + 1));                              // should fail
+describe('#head', () => {
+  it('returns 1 or [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it('returns \'5\' or [\'5\']', () => {
+    assert.strictEqual(head(['5']), '5');
+  })
+});
