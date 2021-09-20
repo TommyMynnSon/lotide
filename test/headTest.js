6704d1d2c0946fe0556dc3d95a368e83a88445e8
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-20 15:20:33 -0700

    Spell check test message for it tests

--- test/headTest.js
+++ test/headTest.js
@@ -3,11 +3,11 @@ const assertEqual = require('../assertEqual');
 const head = require('../head');
 
 describe('#head', () => {
-  it('returns 1 or [1, 2, 3]', () => {
+  it('returns 1 for [1, 2, 3]', () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
 
-  it('returns \'5\' or [\'5\']', () => {
+  it('returns \'5\' for [\'5\']', () => {
     assert.strictEqual(head(['5']), '5');
   })
 });
