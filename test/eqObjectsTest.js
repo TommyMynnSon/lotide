8af218d7849fa2687a42db209bb649fd028ae6ed
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-20 23:33:34 -0700

    Incorporate mocha and chai in eqObjectsTest.js

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,28 +1,32 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqObjects = require('../eqObjects');
 
-console.log('> PRIMITIVE DATA TYPE TESTING <');
+describe('#eqObjects', () => {
+  describe('Primitive data type properties', () => {
+    const ab = { a: "1", b: "2" };
+    const ba = { b: "2", a: "1" };
+    const abc = { a: "1", b: "2", c: "3" };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-const abc = { a: "1", b: "2", c: "3" };
+    it('should return true', () => {
+      assert.isTrue(eqObjects(ab, ba));
+    });
 
-assertEqual(eqObjects(ab, ba), true);       // Test should pass.
-assertEqual(eqObjects(ab, abc), false);     // Test should pass.
-assertEqual(eqObjects(ab, abc), true);      // Test should fail.
-assertEqual(eqObjects(ab, ba), false);      // Test should fail.
+    it('should return false', () => {
+      assert.isFalse(eqObjects(ab, abc));
+    });
+  });
 
-console.log('> ARRAY TYPE TESTING <');
+  describe('Array properties', () => {
+    const cd = { c: "1", d: ["2", 3] };
+    const dc = { d: ["2", 3], c: "1" };
+    const cd2 = { c: "1", d: ["2", 3, 4] };
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-const cd2 = { c: "1", d: ["2", 3, 4] };
-const tommy = { name: 'Tommy', favoriteFoods: ['pizza', 'fish & chips', 'fried chicken'] };
-const robotTommy = { name: 'ROBOT_TOMMY', favoriteFoods: ['pizza', 'fish & chips', 'fried chicken'] };
-const healthyTommy = { name: 'Tommy', favoriteFoods: ['pizza', 'fish & chips', 'kale salad'] };
+    it('should return true', () => {
+      assert.isTrue(eqObjects(cd, dc));
+    });
 
-assertEqual(eqObjects(cd, dc), false);                  // Test should fail.
-assertEqual(eqObjects(cd, cd2), false);                 // Test should pass.
-assertEqual(eqObjects(tommy, tommy), true);             // Test should pass.
-assertEqual(eqObjects(tommy, healthyTommy), false);     // Test should pass.
-assertEqual(eqObjects(tommy, robotTommy), true);        // Test should fail.
+    it('should return false', () => {
+      assert.isFalse(eqObjects(cd, cd2));
+    });
+  });
+});
