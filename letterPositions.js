aa0037e9c54c5142c11a3f5cc036d0948c318d5e
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-20 19:58:59 -0700

    Separate testing within letterPositions.js to letterPositionsTest.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,30 +1,6 @@
-const eqArrays = (arrayOne, arrayTwo) => {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-
-  let sizeOfBothArrays = arrayOne.length;
-
-  for (let i = 0; i < sizeOfBothArrays; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = (arrayOne, arrayTwo) => {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`🎉 Assertion Passed: ${arrayOne} === ${arrayTwo} 🎉`);
-  } else {
-    console.log(`❌ Assertion Failed: ${arrayOne} !== ${arrayTwo} ❌`);
-  }
-};
-
 const letterPositions = (sentence) => {
   const results = {};
-  
+
   for (let i = 0; i < sentence.length; i++) {
     if (results[sentence[i]]) {
       results[sentence[i]].push(i);
@@ -38,21 +14,4 @@ const letterPositions = (sentence) => {
   return results;
 };
 
-// Test/driver code.
-const result = letterPositions('lighthouse in the house');
-const expectedResult = {
-  l: [0],
-  i: [1, 11],
-  g: [2],
-  h: [3, 5, 15, 18],
-  t: [4, 14],
-  o: [6, 19],
-  u: [7, 20],
-  s: [8, 21],
-  e: [9, 16, 22],
-  n: [12]
-};
-
-for (const letter in result) {
-  assertArraysEqual(result[letter], expectedResult[letter]);
-}
+module.exports = letterPositions;
