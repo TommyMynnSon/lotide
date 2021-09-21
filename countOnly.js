f5d33ec25c74efbe32e3438d10448b94edbc0234
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-20 20:21:53 -0700

    Separate testing within countOnly.js to countOnlyTest.js

--- countOnly.js
+++ countOnly.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🎉 Assertion Passed: ${actual} === ${expected} 🎉`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected} ❌`);
-  }
-};
-
-
 const countOnly = (allItems, itemsToCount) => {
   const results = {};
 
@@ -23,22 +14,4 @@ const countOnly = (allItems, itemsToCount) => {
   return results;
 };
 
-// Test/driver code.
-const firstNames = [
-  'Karl',
-  'Salima',
-  'Agouhanna',
-  'Fang',
-  'Kavith',
-  'Jason',
-  'Salima',
-  'Fang',
-  'Joe'
-];
-
-const result1 = countOnly(firstNames, { 'Jason': true, 'Karima': true, 'Fang': true, 'Agouhanna': false });
-
-assertEqual(result1['Jason'], 1);
-assertEqual(result1['Karima'], undefined);
-assertEqual(result1['Fang'], 2);
-assertEqual(result1['Agouhanna'], undefined);
+module.exports = countOnly;
