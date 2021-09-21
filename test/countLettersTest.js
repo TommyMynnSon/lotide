b5bb60890873804e1f7aeef2ac14699116bc5264
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-21 00:04:35 -0700

    Incorporate mocha and chai in countLettersTest.js

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,27 +1,22 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const countLetters = require('../countLetters');
 
-const result = countLetters('lighthouse in the house');
-const expectedResult = {
-  l: 1,
-  i: 2,
-  g: 1,
-  h: 4,
-  t: 2,
-  o: 2,
-  u: 2,
-  s: 2,
-  e: 3,
-  n: 1
-};
+describe('#countLetters', () => {
+  it('should return true', () => {
+    const result = countLetters('lighthouse in the house');
+    const expected = {
+      l: 1,
+      i: 2,
+      g: 1,
+      h: 4,
+      t: 2,
+      o: 2,
+      u: 2,
+      s: 2,
+      e: 3,
+      n: 1
+    };
 
-for (const key in result) {
-  assertEqual(result[key], expectedResult[key]);
-}
-
-// https://www.tutorialspoint.com/how-to-compare-two-objects-in-javascript
-if (JSON.stringify(result) === JSON.stringify(expectedResult)) {
-  console.log('Hard test passed');
-} else {
-  console.log('Hard test failed');
-}
+    assert.deepEqual(result, expected);
+  });
+});
