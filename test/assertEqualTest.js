29632f53791b0b1ad92b496aeb1fdcfccebefb46
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-21 00:16:01 -0700

    Incorporate mocha and chai in assertEqualTest.js

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,12 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-assertEqual("Lighthouse Labs", "Bootcamp");   // non-identical strings
-assertEqual("Tommy", "Tommy");                // identical strings
-assertEqual(10, (10 + 1));                    // non-identical numbers
-assertEqual(1, 1);                            // identical numbers
+describe('#assertEqual', () => {
+  it('should return true', () => {
+    assert.isTrue(assertEqual('Tommy', 'Tommy'));
+  });
+
+  it('should return false', () => {
+    assert.isFalse(assertEqual(10, (10 + 1)));
+  });
+});
