2ea4850a1e8ea32ab1cad43a9b8f331ad7cb5eac
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-20 19:47:46 -0700

    Separate testing within takeUntil.js to takeUntilTest.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,30 +1,3 @@
-const eqArrays = (arrayOne, arrayTwo) => {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-
-  let sizeOfBothArrays = arrayOne.length;
-
-  for (let i = 0; i < sizeOfBothArrays; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = (arrayOne, arrayTwo) => {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`🎉 Assertion Passed: ${arrayOne} === ${arrayTwo} 🎉`);
-  } else {
-    console.log(`❌ Assertion Failed: ${arrayOne} !== ${arrayTwo} ❌`);
-  }
-};
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-
 const takeUntil = (array, callback) => {
   let result = [];
 
@@ -37,5 +10,4 @@ const takeUntil = (array, callback) => {
   }
 };
 
-assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
-assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
+module.exports = takeUntil;
