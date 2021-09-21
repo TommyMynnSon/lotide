097471d68858e7b3e179a921be20fed90db1ec98
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-21 00:19:14 -0700

    Incorporate mocha and chai in assertObjectsEqualTest.js

--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -1,12 +1,16 @@
+const assert = require('chai').assert;
 const assertObjectsEqual = require('../assertObjectsEqual');
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-const cd2 = { c: "1", d: ["2", 3, 4] };
-const tommy = { name: 'Tommy', favoriteFoods: ['pizza', 'fish & chips', 'fried chicken'] };
-const healthyTommy = { name: 'Tommy', favoriteFoods: ['pizza', 'fish & chips', 'kale salad'] };
+describe('#assertObjectsEqual', () => {
+  const cd = { c: "1", d: ["2", 3] };
+  const dc = { d: ["2", 3], c: "1" };
+  const cd2 = { c: "1", d: ["2", 3, 4] };
 
-assertObjectsEqual(cd, cd);               // Test should pass.
-assertObjectsEqual(cd, cd2);              // Test should fail.
-assertObjectsEqual(cd, dc);               // Test should pass.
-assertObjectsEqual(tommy, healthyTommy);  // Test should fail.
+  it('should return true', () => {
+    assert.isTrue(assertObjectsEqual(cd, dc));
+  });
+
+  it('should return false', () => {
+    assert.isFalse(assertObjectsEqual(cd, cd2));
+  });
+});
