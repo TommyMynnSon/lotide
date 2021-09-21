51ed2b3d57b36bcfc29f7180a82e83ecbfb5ac45
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-20 21:39:28 -0700

    Incorporate mocha and chai in takeUntilTest.js

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -1,8 +1,12 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const takeUntil = require('../takeUntil');
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+describe('#takeUntil', () => {
+  it('should return [1, 2, 5, 7, 2] for takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5], x => x < 0)', () => {
+    assert.deepEqual(takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5], x => x < 0), [1, 2, 5, 7, 2]);
+  });
 
-assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
-assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
+  it("should return ['I've', 'been', 'to', 'Hollywood'] for takeUntil(['I've', 'been', 'to', 'Hollywood', ',', 'I've', 'been', 'to', 'Redwood'], x => x === ',')", () => {
+    assert.deepEqual(takeUntil(['I\'ve', 'been', 'to', 'Hollywood', ',', 'I\'ve', 'been', 'to', 'Redwood'], x => x === ','), ["I've", "been", "to", "Hollywood"]);
+  });
+});
