0a32fb03d991c7e4868235602e66d17a3c71b5c5
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-20 23:22:57 -0700

    Incorporate mocha and chai in findKeyTest.js

--- test/findKeyTest.js
+++ test/findKeyTest.js
@@ -1,16 +1,21 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const findKey = require('../findKey');
 
-const testObject = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri": { stars: 3 },
-  "noma": { stars: 2 },
-  "elBulli": { stars: 3 },
-  "Ora": { stars: 2 },
-  "Akelarre": { stars: 3 }
-};
+describe('#findKey', () => {
+  const testObject = {
+    "Blue Hill": { stars: 1 },
+    "Akaleri": { stars: 3 },
+    "noma": { stars: 2 },
+    "elBulli": { stars: 3 },
+    "Ora": { stars: 2 },
+    "Akelarre": { stars: 3 }
+  };
 
-assertEqual(findKey(testObject, x => x.stars === 2), 'noma');       // Test should pass.
-assertEqual(findKey(testObject, x => x.stars === 3), 'Akaleri');    // Test should pass.
-assertEqual(findKey(testObject, x => x.stars === 1), 'Blue Hill');  // Test should pass.
-assertEqual(findKey(testObject, x => x.stars === 2), 'Ora');        // Test should fail.
+  it('should return \'noma\'', () => {
+    assert.equal(findKey(testObject, x => x.stars === 2), 'noma');
+  });
+
+  it('should return \'Akaleri\'', () => {
+    assert.equal(findKey(testObject, x => x.stars === 3), 'Akaleri');
+  });
+});
